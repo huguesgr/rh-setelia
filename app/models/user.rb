742ea053954af27c903e31161f2e2b883dde2934
Hugues Granger <hugues.granger@setelia.com>
2014-08-29 11:31:53 +0200

    First steps...

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+end
