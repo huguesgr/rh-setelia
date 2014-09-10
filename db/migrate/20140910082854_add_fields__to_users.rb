8192b5351d71554fd3559f845455a5ca21160aa6
Hugues Granger <hugues.granger@setelia.com>
2014-09-10 23:07:07 +0200

    Trying to solve a Heroku bug.

--- db/migrate/20140910082854_add_fields__to_users.rb
+++ db/migrate/20140910082854_add_fields__to_users.rb
@@ -1,7 +1,7 @@
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :interview_result, :integer
-    add_column :users, :interview_state, :integer
-    add_column :users, :comment, :string
+    #add_column :users, :interview_result, :integer
+    #add_column :users, :interview_state, :integer
+    #add_column :users, :comment, :string
   end
 end
