92245937192a78eab5087997f57661ef37e84689
Hugues Granger <hugues.granger@setelia.com>
2014-11-12 14:55:43 +0100

    Fix #21.3

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,28 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_admin!
+
   def new
     @profile = Profile.new
   end
   def index
     @profiles = Profile.order(:label).all
   end
-
+  def edit
+    @profile = Profile.find(params[:id])
+  end 
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "#{Profile.model_name.human} modifié."
+      redirect_to profiles_path
+    else
+      render 'edit'
+    end
+  end
   def create
     @profile = Profile.new(profile_params)
     if @profile.save
-      flash[:success] = "#{Profile.model_name.human} ajoutée."
+      flash[:success] = "#{Profile.model_name.human} ajouté."
       redirect_to profiles_path
     else
       render 'new'
