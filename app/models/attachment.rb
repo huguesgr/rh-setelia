8e97c0b74792a0eaaaeb95c150ee3205f929a752
Hugues Granger <hugues.granger@setelia.com>
2014-11-18 22:52:40 +0100

    Fix #31.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,5 +2,6 @@ class Attachment < ActiveRecord::Base
   belongs_to :user
   has_attached_file :resume
 
+  validates :resume, presence: true
   validates_attachment_file_name :resume, :matches => [/pdf\Z/, /docx?\Z/, /odt\Z/, /rtf\Z/, /txt\Z/]
 end
