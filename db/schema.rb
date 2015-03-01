0f0e2162a2686e76f682363c78e912c0ebfd13cc
Hugues GRANGER <hugues.granger@setelia.com>
2015-03-01 23:24:41 +0100

    Favicon.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141201133200) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
