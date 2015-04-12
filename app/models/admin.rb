d0f950e20ab007774cb5b12ada484195f9df1979
Hugues Granger <hugues.granger@gmail.Com>
2015-04-12 22:04:25 +0200

    Enabling lost password function.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable,
          :password_expirable, :secure_validatable, :password_archivable
 
   def active_for_authentication? 
