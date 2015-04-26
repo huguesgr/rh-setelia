571c2765e509aa44264626595ee199c1cb2596c4
Hugues Granger <hugues.granger@gmail.Com>
2015-04-26 18:00:38 +0200

    Fix #40.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,13 @@
 //= require rails-timeago
 //= require locales/jquery.timeago.fr.js
+
+$(document).ready(function(){
+	$('button#check_all').on("click", function(){
+		if($('input[name="user[area_ids][]"]').is(':checked')) {
+			$('input[name="user[area_ids][]"]').prop('checked', false);
+		}
+		else {
+			$('input[name="user[area_ids][]"]').prop('checked', true);
+		};
+	});
+});
